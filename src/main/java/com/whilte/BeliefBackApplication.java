02ed39f7829a2c6d1e771e4b335295cfd69fb06e
whilte <505200470@qq.com>
2017-12-27 09:57:31 +0800

    项目初始化

---
+++ src/main/java/com/whilte/BeliefBackApplication.java
@@ -0,0 +1,12 @@
+package com.whilte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeliefBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeliefBackApplication.class, args);
+	}
+}
